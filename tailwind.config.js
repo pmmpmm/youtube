972f5621bb9e2011a39bde2962938d5b8ce24dcb
minseop.lee <minseop.lee@gi-vita.io>
2024-02-28 15:58:19 +0900

    refactor: [RY-XXXX] add typescript
    a
    
    ㅁ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: [
+    './src/**/*.{js,jsx}',
+    './src-ts/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {},
     container: {
