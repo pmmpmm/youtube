277be15bfcc0f69fac12b2ce3dc11688d4546881
minseop.lee <minseop.lee@gi-vita.io>
2024-03-04 20:59:15 +0900

    refactor: videos

---
+++ src/pages/Search.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useNavContext } from '../context/NavContext';
+import Loading from '../components/Loading/Loading';
+import Error from '../components/ErrorComponent/Error';
+import VideoCard from '../components/VideoCard/VideoCard';
+
+export default function Search() {
+  const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+
+  const getSearch = ({ queryKey }) => {
+    return youtube.search(queryKey[1])
+  }
+
+  // Q1 : nav의 '음악' 혹은 '예능' 클릭 시 컨텐츠 전환
+  const {
+    data: videos,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: getSearch,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      {error && <Error />}
+      {videos && (
+        <ul className={`videos ${keyword ? 'list' : ''}`}>
+          {videos.map((video) => {
+            return <VideoCard key={video.id} video={video} style={`${keyword ? 'list' : ''}`} />;
+          })}
+        </ul>
+      )}
+    </>
+  );
+}
