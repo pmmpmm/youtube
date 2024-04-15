e9fcfed4a3948c9f6b888e3dd181551000403700
kyoung hwan <pmmpmm85@gmail.com>
2024-04-16 00:31:12 +0900

    RY-68 refactor: Header

--- src-ts/App.tsx
+++ src-ts/App.tsx
@@ -2,34 +2,37 @@ import queryClient from '@/service/QueryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Videos from './pages/Videos';
-import VideoDetail from './pages/VideoDetail';
-import NavVideos from './pages/NavVideos';
-import PopularVideos from './pages/PopularVideos';
+import VideoDetail from '@/pages/VideoDetail';
+import NavVideos from '@/pages/NavVideos';
+//
+import PopularVideos from '@/pages/PopularVideos';
+import SearchVideos from '@/pages/SearchVideos';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-        <RouterProvider router={createBrowserRouter([
-        // {
-        //   path: '/',
-        //   element: <Root />,
-        //   errorElement: <NotFound />,
-        //   children: [
-        //     { index: true, element: <Videos /> },
-        //     { path: 'videos', element: <Videos /> },
-        //     { path: 'videos/:keyword', element: <Videos /> },
-        //     { path: 'videos/watch/:id', element: <VideoDetail /> },
-        //     { path: 'page/:navName', element: <NavVideos /> },
-        //   ],
-        // },
-        { path: '/', element: <PopularVideos /> },
-        { path: 'videos', element: <Videos /> },
-        { path: 'videos/:keyword', element: <Videos /> },
-        { path: 'videos/watch/:id', element: <VideoDetail /> },
-        { path: 'page/:navName', element: <NavVideos /> },
-      ])} />
+      <RouterProvider
+        router={createBrowserRouter([
+          // {
+          //   path: '/',
+          //   element: <Root />,
+          //   errorElement: <NotFound />,
+          //   children: [
+          //     { index: true, element: <Videos /> },
+          //     { path: 'videos', element: <Videos /> },
+          //     { path: 'videos/:keyword', element: <Videos /> },
+          //     { path: 'videos/watch/:id', element: <VideoDetail /> },
+          //     { path: 'page/:navName', element: <NavVideos /> },
+          //   ],
+          // },
+          { path: '/', element: <PopularVideos /> },
+          { path: 'videos', element: <PopularVideos /> },
+          { path: 'videos/:keyword', element: <SearchVideos /> },
+          { path: 'videos/watch/:id', element: <VideoDetail /> },
+          { path: 'page/:navName', element: <NavVideos /> },
+        ])}
+      />
     </QueryClientProvider>
   );
 };
--- src-ts/components/features/Header.tsx
+++ src-ts/components/features/Header.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, KeyboardEvent } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import {
-  IoIosSearch,
-  IoIosSunny,
-  IoIosMoon,
-  IoIosArrowRoundBack,
-  IoIosClose,
-} from 'react-icons/io';
+import { IoIosSearch, IoIosArrowRoundBack, IoIosClose } from 'react-icons/io';
 import Logo from '@/components/ui/Logo';
+import DarkModeBtn from '@/components/ui/DarkModeBtn';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -22,14 +17,16 @@ const Header = () => {
     focus
       ? searchInp.current && searchInp.current.focus()
       : searchInp.current && searchInp.current.blur();
-
-    // 검색 결과 화면: 검색폼 노출
   }, [keyword, focus]);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     if (text.trim().length > 0) navigate(`/videos/${text.trim()}`);
     setFocus(false);
+  };
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
   };
   return (
     <>
@@ -38,27 +35,24 @@ const Header = () => {
           <Logo />
         </h1>
       </Link>
-      <div className='formWrap flex flex-initial basis-[62%] justify-end sm:justify-center md:basis-6/12 lg:basis-5/12'>
-        {/* form */}
+      <div className='flex flex-initial basis-[62%] justify-end sm:justify-center md:basis-6/12 lg:basis-5/12'>
         <div
-          className={`formBox items-center w-full h-full px-4 bg-white absolute top-0 left-0 -z-10 
+          className={`items-center w-full h-full px-4 bg-white absolute top-0 left-0 -z-10 
             sm:flex sm:p-0 sm:relative sm:z-10 dark:bg-neutral-900
             ${focus ? 'flex z-50' : 'hidden'}`}
         >
           <button
-            onClick={() => {
-              setFocus(false);
-            }}
+            onClick={() => setFocus(false)}
             className='btn close flex-none block w-10 h-10 sm:hidden'
             aria-label='모바일 화면 검색 폼 숨김 버튼'
           >
             <IoIosArrowRoundBack className='inline-block w-7 h-7 text-neutral-700 dark:text-white' />
           </button>
-          <form
-            onSubmit={handleSubmit}
+          {/* form */}
+          <div
             onFocus={() => setFocus(true)}
             onBlur={() => setFocus(false)}
-            className={`form flex w-full h-10 border border-solid border-neutral-200 rounded-full dark:border-neutral-700
+            className={`flex w-full h-10 border border-solid border-neutral-200 rounded-full dark:border-neutral-700
               ${focus ? 'border-transparent shadow-form-focus dark:border-neutral-100' : ''}`}
           >
             <input
@@ -68,6 +62,7 @@ const Header = () => {
               value={text}
               onChange={(e) => setText(e.target.value)}
               className='flex-initial w-full h-10 px-6 font-normal text-neutral-950 bg-transparent border-transparent placeholder:text-neutral-400 dark:text-neutral-100 dark:font-light dark:placeholder:text-neutral-300'
+              onKeyPress={handleKeyPress}
             />
             {text && (
               <button
@@ -79,10 +74,15 @@ const Header = () => {
                 <IoIosClose className='inline-block w-7 h-7 text-neutral-700 dark:text-neutral-50' />
               </button>
             )}
-            <button type='submit' aria-label='검색 버튼' className='flex-none w-16'>
+            <button
+              type='button'
+              onClick={handleSubmit}
+              aria-label='검색 버튼'
+              className='flex-none w-16'
+            >
               <IoIosSearch className='inline-block w-7 h-7 text-neutral-700 dark:text-white' />
             </button>
-          </form>
+          </div>
         </div>
         {/* 모바일화면 - 검색폼 노출 버튼 */}
         <button
@@ -97,23 +97,7 @@ const Header = () => {
       </div>
 
       {/* 다크모드 버튼 */}
-      <button className='flex-none w-10 h-10 text-[0]'>
-        <span className='block text-center'>
-          라이트모드
-          <IoIosSunny className='inline-block -ml-1 w-[66%] h-[66%] text-neutral-700 sm:w-[64%] sm:h-[64%] dark:text-neutral-50' />
-        </span>
-        {/* {dark ? (
-          <span className='block text-center'>
-            라이트모드
-            <IoIosSunny className='inline-block -ml-1 w-[66%] h-[66%] text-neutral-700 sm:w-[64%] sm:h-[64%] dark:text-neutral-50' />
-          </span>
-        ) : (
-          <span className='block text-center'>
-            다크모드
-            <IoIosMoon className='inline-block -ml-1 w-[66%] h-[66%] text-neutral-700 sm:w-[64%] sm:h-[64%] dark:text-neutral-50' />
-          </span>
-        )} */}
-      </button>
+      <DarkModeBtn />
     </>
   );
 };
