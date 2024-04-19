d4cac03cead1ed93ccb76ecde80b3c0bfa41cdf7
kyoung hwan <pmmpmm85@gmail.com>
2024-04-19 10:39:17 +0900

    RY-58 refactor: video card divide

--- src-ts/components/ui/VideoCardContainer.tsx
+++ src-ts/components/ui/VideoCardContainer.tsx
@@ -1,4 +1,9 @@
 const VideoCardContainer = ({ children }: { children: React.ReactNode }) => {
+<<<<<<< HEAD
+=======
+  console.log('VideoCardContainer');
+
+>>>>>>> 0c6215d (RY-58 refactor: video card divide)
   return (
     <ul className='grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-4 sm:gap-y-10 lg:grid-cols-3 2xl:grid-cols-4'>
       {children}
